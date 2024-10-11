990231daa5789519860eb26c9668c33edbfcbc5b
moonissmiling <122709554+m00nisSmiling@users.noreply.github.com>
2024-10-11 18:36:21 +0630

    Create ktn.py

---
+++ tools/ktn.py
@@ -0,0 +1,10 @@
+import os 
+import sys
+
+aa = sys.argv[1]
+
+file2 = open(aa).read()
+file3 = file2.splitlines()
+
+for i in file3:
+ os.system(f"sudo docker run projectdiscovery/katana:latest -u {i} -d 5 waybackarchive,commoncrawl,alienvault -kf -jc -fx -ef woff,css,png,svg,jpeg,jpg,gif,svg,woff2 | tee -a /home/m00nissmiling/prod/all_endpoints.txt")
