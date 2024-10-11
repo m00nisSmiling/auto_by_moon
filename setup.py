e1fb7c0e0b64de79bccc2f60e10d650907f9af53
moonissmiling <122709554+m00nisSmiling@users.noreply.github.com>
2024-10-11 18:30:10 +0630

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os
+
+print("Requirements are installing ................")
+os.system("pip install termcolor")
+os.system("sudo apt install docker.io")
+os.system("docker pull projectdiscovery/katana:latest")
+os.system("sudo apt install httpx-toolkit") 
